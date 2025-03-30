1097a18eab834565f8a024ca037772aa1f7dc570
totalapping <hemantyadav9484@gmail.com>
2025-03-30 21:02:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,54 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
+const express = require('express');
 
-// ENV Variables
-const VIDEO_URL = process.env.VIDEO_URL;
-const RTMP_URL = "rtmp://a.rtmp.youtube.com/live2";
-const STREAM_KEY = process.env.STREAM_KEY;
+const app = express();
+const port = process.env.PORT || 3000;
 
-// FFmpeg Command to Stream from Google Drive
-const cmd = [
-  'ffmpeg',
-  '-re',
-  '-stream_loop', '-1',
-  '-i', `"${VIDEO_URL}"`,
-  '-vf', 'fps=30,scale=1280:720',
-  '-c:v', 'libx264',
-  '-preset', 'ultrafast',
-  '-g', '60',
-  '-keyint_min', '30',
-  '-b:v', '3000k',
-  '-bufsize', '6000k',
-  '-maxrate', '3000k',
-  '-c:a', 'aac',
-  '-b:a', '128k',
-  '-f', 'flv',
-  `"${RTMP_URL}/${STREAM_KEY}"`
-].join(' ');
+// Google Drive Downloadable Link
+const videoURL = "https://drive.google.com/uc?export=download&id=1E312l5AF5Obg8oVn6HCZ_uAmjHUuUwYu";
 
-console.log("Starting Stream...");
-const stream = exec(cmd, (error, stdout, stderr) => {
-  if (error) {
-    console.error("Error:", stderr);
-    process.exit(1);
-  }
+app.get('/', (req, res) => {
+  res.send('Streaming is live!');
 });
 
-stream.stderr.on('data', (data) => {
-  console.log(data.toString());
+function startStream() {
+  console.log('Starting Stream...');
+
+  const ffmpeg = spawn('ffmpeg', [
+    '-re',
+    '-i', videoURL,
+    '-c:v', 'libx264',
+    '-preset', 'ultrafast',
+    '-b:v', '4500k',
+    '-maxrate', '4500k',
+    '-bufsize', '9000k',
+    '-c:a', 'aac',
+    '-b:a', '128k',
+    '-f', 'flv',
+    'rtmp://a.rtmp.youtube.com/live2/YOUR_STREAM_KEY'
+  ]);
+
+  ffmpeg.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  ffmpeg.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  ffmpeg.on('close', (code) => {
+    console.log(`FFmpeg exited with code ${code}`);
+    if (code !== 0) {
+      console.error('Stream crashed. Restarting...');
+      setTimeout(startStream, 5000); // 5 second delay before restart
+    }
+  });
+}
+
+// Start the stream
+startStream();
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
